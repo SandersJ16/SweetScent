1abd51c08a0f7a8d496ede67cb2007c65a6cfec3
Justin L Sanders <justin@pointw.com>
2020-01-21 22:08:02 -0600

    Make Positive test cases work

--- src/Sniffs/Braces/AllowSingleLineClassSniff.php
+++ src/Sniffs/Braces/AllowSingleLineClassSniff.php
@@ -5,13 +5,50 @@ namespace Sandersj16\SweetScent\Sniffs\Braces;
 use PHP_CodeSniffer\Sniffs\Sniff;
 use PHP_CodeSniffer\Files\File;
 
-class AllowSingleLineClassSniff implements Sniff {
-    public function register() {
+class AllowSingleLineClassSniff implements Sniff
+{
+    const INCORRECT_CLASS_BRACKETS = 'INCORRECT_CLASS_BRACKETS';
+
+    public function register()
+    {
         return [T_CLASS];
     }
 
-    public function process(File $phpcsFile, $currentTokenIndex) {
+    public function process(File $phpcsFile, $currentTokenIndex)
+    {
         $tokens = $phpcsFile->getTokens();
-        var_dump($tokens[$currentTokenIndex]);
+        $variablePointer = $currentTokenIndex;
+        $new_line = false;
+
+        // Iterate through the tokens until you find the opening bracket of the class definition
+        // Keep track of if we encountered a newline before getting to the opening bracket
+        do {
+            $variablePointer++;
+            $token = $tokens[$variablePointer];
+            $token_code = $token['code'];
+            if ($token_code == T_WHITESPACE && strpos($token['content'], "\n") !== false) {
+                $new_line = true;
+            }
+        } while ($token_code != T_OPEN_CURLY_BRACKET);
+
+
+        $i = 1;
+        $next_token = $tokens[$variablePointer + $i];
+
+        // If we encountered a newline then make sure that the class has a body
+        if ($new_line) {
+            do {
+                if ($next_token['code'] == T_CLOSE_CURLY_BRACKET) {
+                    $phpcsFile->addError('Empty class declaration with braces on newline', $variablePointer, self::INCORRECT_CLASS_BRACKETS);
+                    break;
+                }
+                ++$i;
+                $next_token = $tokens[$variablePointer + $i];
+            } while ($next_token['code'] != T_WHITESPACE);
+
+        // If we didn't encounter a newline make sure the next character after the opneing bracket is a closing bracket
+        } elseif (!$new_line && $next_token['code'] != T_CLOSE_CURLY_BRACKET) {
+            $phpcsFile->addError('Starting class brace on same line of non-empty class', $variablePointer, self::INCORRECT_CLASS_BRACKETS);
+        }
     }
 }
--- tests/Sniffs/Braces/data/classSingleLineDefinitionNoErrors.php
+++ tests/Sniffs/Braces/data/classSingleLineDefinitionNoErrors.php
@@ -7,3 +7,37 @@ class Bar extends Fubar {}
 class X implements Y {}
 
 class A extends B implements C {}
+
+class Test1
+{
+    public  function test()
+    {
+
+    }
+}
+
+class Test2 extends Fubar
+{
+    public  function test()
+    {
+
+    }
+}
+
+class Test3 implements Y
+{
+    public  function test()
+    {
+
+    }
+}
+
+class Test extends Funbar implements Y
+{
+    public  function test()
+    {
+
+    }
+}
+
+
